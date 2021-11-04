f5d3f98a67c87fc97c500b4bec639df3243fed1f
huynhminhtien123 <93719896+huynhminhtien123@users.noreply.github.com>
2021-11-04 21:13:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "Thế Giới của Anh",
+    introTitle: 'Nguyễn (Thị) Phương Thảo',
+    introDesc: `Thì mình cũng nhắn tin được 1 tháng rồi đúng không nè
+Nghe thì ít thiệt nhưng mà, khoảng thời gian qua anh thật lòng đã cảm nhận được cảm giác vui là thế nào, cảm giác hạnh phúc là thế nào. Hình như em cũng biết rồi nhưng mà anh trước khi gặp em, anh bị trầm cảm khá nặng, ý định tự tử cũng có, nào là chuyện gia đình đến bạn bè, anh điều không thể mở lòng với ai. Nhưng đến khi nhắn tin với em, mặc dù còn khá sớm nhưng lòng anh đã cảm thấy nhẹ nhàng hơn hẳn, ừ thì chưa gặp mặt nhưng  nhắn tin như vầy thôi mà anh đã lỡ yêu em mất rồi.  Em là 1 người con gái hiền lành, hồn nhiên, vui tính, dứt khoác, nghĩ gì nói đó, hơi bị vô tư, thoải mái(đôi khi thoải mái quá 😒), đôi lúc láo 🙂 nhưng vẫn ngoan và hồn nhiên 1 cách đáng yêu. Anh biết là càng về sau anh sẽ chỉ càng yêu em thêm thôi. Những điều em cho là xấu, không tốt ở bản thân thì điều đó không sao đâu, anh cũng có những điều đó, không ai hoàn hảo cả, nên là khi mình sánh bước bên nhau thì sẽ có thêm động lực để đổi thay vì nhau, phát triển vì nhau. Anh dù chưa có mọi thứ trong tay, chưa '' thành công'' nhưng anh muốn em là người đồng hành cùng anh trên con đường thành công đó, cùng nhau gây dựng sự nghiệp của cả 2. Anh  muốn che chở, bảo vệ, muốn bên em lúc vui, lúc buồn, lúc thăng và lúc trầm.
+Ưm anh không biết là có thiếu gì nữa không nhưng cái anh biết bây giờ là anh yêu em thật lòng, anh thương em thật lòng, và anh muốn ở bên em thật lòng.
+Nên là Em ơi, anh có thể làm người yêu của Nguyễn (Thị) Phương Thảo 
+`,
+    btnIntro: ':V',
+    title: 'Anh Đỗ Phú Phước có thể làm người yêu của em, Nguyễn (Thị) Phương Thảo ',
+    desc: 'Được không? ',
+    btnYes: 'Dạ',
+    btnNo: 'Hong :(',
+    question: 'Cảm ơn em',
+    btnReply: 'Gửi cho Phú',
+    reply: 'Trầm cảm',
+    mess: 'Yêu em vlllllll :)))))',
+    messDesc: 'Hẹn em 3 tháng nữa gặp mặt nhau nhé',
+    btnAccept: 'dạ :3',
+    messLink: 'https://www.facebook.com/Xcalliver/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
